6ddec728356e4b0ee31d586f0c5236292e059d9a
Joshua Liang <liangjos@shernet.sheridancollege.ca>
2025-02-06 11:21:03 -0500

    attempting to do the third

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 
 public class Player_Movement : MonoBehaviour
 {
 
     public float speed = 20;
+    public Vector2 banana_position;
+    public GameObject Gameobject;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +20,24 @@ public class Player_Movement : MonoBehaviour
     {
 
         Vector2 position = transform.position;
+        Vector2 banana_position = Gameobject.transform.position;
+
 
 
         position.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         position.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        if (Vector2.Distance(position, banana_position) > 10)
+        {
+
+            print("bnanan");
+
+        }
+        else {
+
+            print("no babanbna");
+
+        }
 
         transform.position = position;
     }
